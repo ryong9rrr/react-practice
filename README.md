4fc0432efdcffc1c661f31409569c9e9c631aff4
yong <tkddbs1644@gmail.com>
2022-07-13 19:07:24 +0900

    12장: immer 라이브러리 사용하기

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-리액트 연습용 저장소입니다.
+# immer 라이브러리 사용해서 불변성을 유지하기
+
+> [immer 공식문서 참고](https://immerjs.github.io/immer/example-setstate/)
+
+1. `yarn add immer`
--- src/App.js
+++ src/App.js
@@ -1,5 +1,112 @@
+import produce from 'immer'
+import { useCallback, useRef, useState } from 'react'
+
 function App() {
-  return <h1>Hello react!</h1>;
+  const nextId = useRef(1)
+  const [form, setForm] = useState({ name: '', username: '' })
+  const [data, setData] = useState({
+    array: [],
+    uselessValue: null,
+  })
+
+  const onChange = useCallback((e) => {
+    const { name, value } = e.target
+    // setForm({
+    //   ...form,
+    //   [name]: [value],
+    // })
+
+    // setForm(
+    //   produce(form, (draft) => {
+    //     draft[name] = value
+    //   })
+    // )
+
+    setForm(
+      produce((draft) => {
+        draft[name] = value
+      })
+    )
+  }, [])
+
+  const onSubmit = useCallback(
+    (e) => {
+      e.preventDefault()
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username,
+      }
+
+      // setData({
+      //   ...data,
+      //   array: [...data.array, info],
+      // })
+
+      // setData(
+      //   produce(data, (draft) => {
+      //     draft.array.push(info)
+      //   })
+      // )
+
+      setData(
+        produce((draft) => {
+          draft.array.push(info)
+        })
+      )
+
+      setForm({
+        name: '',
+        username: '',
+      })
+      nextId.current += 1
+    },
+    [form.name, form.username]
+  )
+
+  const onRemove = useCallback((id) => {
+    // setData({
+    //   ...data,
+    //   array: data.array.filter((info) => info.id !== id),
+    // })
+
+    // setData(
+    //   produce(data, (draft) => {
+    //     draft.array.splice(
+    //       draft.array.findIndex((info) => info.id === id),
+    //       1
+    //     )
+    //   })
+    // )
+
+    setData(
+      produce((draft) => {
+        draft.array.splice(
+          draft.array.findIndex((info) => info.id === id),
+          1
+        )
+      })
+    )
+  }, [])
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input name="username" placeholder="아이디" value={form.username} onChange={onChange} />
+        <input name="name" placeholder="이름" value={form.name} onChange={onChange} />
+        <button type="submit">등록</button>
+      </form>
+      <div>
+        <ul>
+          {data.array.map((info) => (
+            <li key={info.id} onClick={() => onRemove(info.id)}>
+              {info.username} ({info.name})
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
