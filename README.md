d5c824202e18f75eb92577f4ee652d6932851941
yong <tkddbs1644@gmail.com>
2022-07-15 23:38:41 +0900

    react-router-dom v6 튜토리얼

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-리액트 연습용 저장소입니다.
+# react-router-dom v6 튜토리얼
+
+> https://reactrouter.com/docs/en/v6/getting-started/tutorial
+
+검색 목록을 필터링하고 링크를 이동한 다음
+
+1. 목록을 필터링 된 상태로 유지하고
+2. URL의 매개변수를 유지하고 싶다? -> "사용자 지정 동작" 부분 볼 것
---
+++ src/routes/invoice.jsx
@@ -0,0 +1,30 @@
+import { useLocation, useNavigate, useParams } from 'react-router-dom'
+import { deleteInvoice, getInvoice } from '../data'
+
+export default function Invoice() {
+  let navigate = useNavigate()
+  let location = useLocation()
+  let params = useParams()
+  let invoice = getInvoice(parseInt(params.invoiceId, 10)) || null
+  return invoice ? (
+    <main style={{ padding: '1rem' }}>
+      <h2>Total Due: {invoice.amount}</h2>
+      <p>
+        {invoice.name}: {invoice.number}
+      </p>
+      <p>Due Date: {invoice.due}</p>
+      <p>
+        <button
+          onClick={() => {
+            deleteInvoice(invoice.number)
+            navigate('/invoices' + location.search)
+          }}
+        >
+          Delete
+        </button>
+      </p>
+    </main>
+  ) : (
+    <h1>잘못된 경로입니다</h1>
+  )
+}
